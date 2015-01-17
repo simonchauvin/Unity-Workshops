0813b042413f68d0e1313a23e212780bc8d928d7
Simon Chauvin <contact@simonchauvin.com>
2015-01-17 19:30:26 +0100

    Planet gravity and ship movement.

--- Gravity/Assets/Scripts/Planet.cs
+++ Gravity/Assets/Scripts/Planet.cs
@@ -3,16 +3,20 @@ using System.Collections;
 
 public class Planet : MonoBehaviour
 {
+	private Transform ship;
 
 	// Use this for initialization
 	void Start ()
 	{
-		
+		ship = GameObject.Find("Ship").transform;
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		
+		float masses = rigidbody2D.mass * ship.rigidbody2D.mass;
+		float g = 6.67f;
+		Vector3 distance = ship.position - transform.position;
+		ship.rigidbody2D.AddForce(-g * (masses / (distance.magnitude * distance.magnitude)) * distance.normalized);
 	}
 }
--- Gravity/Assets/Scripts/Ship.cs
+++ Gravity/Assets/Scripts/Ship.cs
@@ -13,6 +13,19 @@ public class Ship : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
+		if (Input.GetKey(KeyCode.UpArrow))
+		{
+			rigidbody2D.AddForce(transform.up * 10f);
+		}
 
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.RotateAround(transform.position, Vector3.forward, 5f);
+		}
+
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.RotateAround(transform.position, Vector3.forward, -5f);
+		}
 	}
 }
